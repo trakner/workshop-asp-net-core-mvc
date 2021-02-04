f717b9025811c61e7d66bf8a242885a6a5ca0516
Luan Caetano <luanlcr@hotmail.com>
2021-02-04 20:28:12 -0300

    First Model-Controller-View - Department

--- SallesWebMvc/Controllers/HomeController.cs
+++ SallesWebMvc/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SallesWebMvc.Models;
+using SallesWebMvc.Models.ViewModels;
 
 namespace SallesWebMvc.Controllers
 {
